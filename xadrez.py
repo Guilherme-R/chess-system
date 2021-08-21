4bd8e844c43fd002daa7065e937f05d9309cb5a5
Guilherme-R <guilherme.r.bs@outlook.com>
2021-08-21 11:56:00 -0300

    Classes ChessPiece, ChessMatch, Color e UI. Printando no console o tabuleiro

--- xadrez.py
+++ xadrez.py
@@ -1,5 +1,12 @@
+from enum import Enum
+
+
 def createMatriz(rows, columns):
-    return [[None]*columns for _ in range(rows)]
+    return [[Piece()]*columns for _ in range(rows)]
+
+
+def createMatrizChessPiece(rows, columns):
+    return [[ChessPiece()]*columns for _ in range(rows)]
 
 
 class Position():
@@ -17,13 +24,65 @@ class Board():
         self._columns = columns
         self._pieces = createMatriz(rows, columns)
 
+    def Piece(self, row, column):
+        return self._pieces[row][column]
+
+    def PiecePosition(self, position):
+        return self._pieces[position._rows][position._columns]
+
 
 class Piece():
     def __init__(self, board=None):
         self._position = None
-        # self._color = color
         self._board = board
 
 
+class Color(Enum):
+    WHITE = 1,
+    BLACK = 2,
+
+
+class ChessPiece(Piece):
+    def __init__(self, board=None, color=None, piece=None):
+        if piece:
+            super().__init__(piece._board)
+        else:
+            super().__init__(board)
+            self._color = color
+
+
+class ChessMatch():
+    def __init__(self):
+        self._board = Board(8, 8)
+
+    def getPieces(self):
+        mat = createMatrizChessPiece(self._board._rows, self._board._columns)
+        for i in range(self._board._rows):
+            for j in range(self._board._columns):
+                mat[i][j] = ChessPiece(piece=self._board._pieces[i][j])
+        return mat
+
+
+class UI():
+
+    @staticmethod
+    def printBoard(pieces):
+        for i in range(len(pieces)):
+            print(f'{8 - i} ', end='')
+            for j in range(len(pieces)):
+                UI.printPiece(pieces[i][j])
+            print()
+        print('  a b c d e f g h')
+
+    @staticmethod
+    def printPiece(piece):
+        if piece._board == None:
+            print('-', end='')
+        else:
+            print(piece, end='')
+        print(' ', end='')
+
+
 if __name__ == '__main__':
-    print(Position(3,5))
+    chessMatch = ChessMatch()
+    UI.printBoard(chessMatch.getPieces())
